4b5b04cd7459ebe7838f0d39b0177cb082e68186
aurelienbernard <aurelienpoolen@gmail.com>
2019-12-23 22:42:48 +0100

    PUSH pops are good now

--- introsort.asm
+++ introsort.asm
@@ -6,30 +6,34 @@
 sort:
   PUSH(LP) PUSH(BP)
   ADDC(SP,0,BP)
-  PUSH(R1)
-  PUSH(R2)
-  PUSH(R0)
   PUSH(R20)
+  PUSH(R0)|;hold returns
+  PUSH(R1)|;hold array
+  PUSH(R2)|;hold size
+  PUSH(R3)|; will hold maxd
   LD(BP,-12,R1) |; load array
   LD(BP,-16,R2) |; load size
   CMPEQ(R2,R31,R20)|; evaluate SIZE == 0
   BNE(R20, return) |; if r20 == 1 return
-  PUSH(R2)|;Push the size
+  PUSH(R2)|;Push the size arg1
   BEQ(R31, log2, LP)
-  POP(R2)
-  ADDC(R0,0,R3)
-  POP(R0)
-  PUSH(R3)
+  POP(R2) |;pop the argument
+  ADDC(R0,0,R3) |;move the result to R3
+  PUSH(R3)|;push the arguments
   PUSH(R2)
   PUSH(R1)
-  BEQ(R31, introsort, LP)
-  BEQ(R31, return, R31)
+  BEQ(R31, introsort, LP)|;call the sort
+  POP(R1)|;pop the arguments
+  POP(R2)
+  POP(R3)
+  BEQ(R31, return, R31)|;return
 
 return:
-  POP(R20)
-  POP(R0)
+  POP(R3)
   POP(R2)
   POP(R1)
+  POP(R0)
+  POP(R20)
   ADDC(BP, 0, SP)
   POP(BP)|;POP BP LP
   POP(LP)
